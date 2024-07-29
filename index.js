3fc8ec56e60df521a716b4e1bf52d55ba0b285eb
Moez Khalid <29011087+mightymozdef@users.noreply.github.com>
2024-07-28 22:09:47 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(req.body),
+    ].join(" ");
+  })
+);
+
+let phonebook = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (req, res) => {
+  res.json(phonebook);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = phonebook.find((p) => p.id === id);
+  if (person) res.json(person);
+  else res.status(404).end();
+});
+
+app.get("/info", (req, res) => {
+  const people = phonebook.length;
+  const requestTime = new Date().toString();
+  res.send(
+    `<p>Phonebook has info for ${people} people</p><br /><p>${requestTime}</p>`
+  );
+});
+
+const generateId = () => {
+  const maxId =
+    phonebook.length > 0 ? Math.max(...phonebook.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "the name or number is missing",
+    });
+  } else if (phonebook.some((person) => person.name === body.name)) {
+    return res.status(400).json({
+      error: "the name already exists in the phonebook",
+    });
+  }
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  phonebook = phonebook.concat(person);
+  res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  phonebook = phonebook.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
